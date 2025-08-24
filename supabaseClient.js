a47e98c65abf344edc04c56338c2b88f2e53d178
yangycl <yangyangyclee@gmail.com>
2025-08-24 09:20:49 +0800

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+  'https://vfqedrijjknhydradytf.supabase.co',
+  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICRjDGbwdHVOx1Kq4Pk/AYpm7d2ha/w9cGPfDBwH7BnI'
+);
