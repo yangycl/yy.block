94750e9dac28dc96071e895cba379c48eab4a06b
yangycl <yangyangyclee@gmail.com>
2025-08-24 09:22:00 +0800

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   'https://vfqedrijjknhydradytf.supabase.co',
-  'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICRjDGbwdHVOx1Kq4Pk/AYpm7d2ha/w9cGPfDBwH7BnI'
+  ''
 );
